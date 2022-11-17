33b7af33017b7b48c27cf3c02b4b2b69f3b6336f
MellyGray <melinaabril@gmail.com>
2022-11-17 20:24:09 +0100

    Change test body to json format

--- src/test/java/mowers/controller/controllers/MowersInstructionPostController.java
+++ src/test/java/mowers/controller/controllers/MowersInstructionPostController.java
@@ -7,8 +7,8 @@ import mowers.controller.RequestTestCase;
 public class MowersInstructionPostController extends RequestTestCase{
     @Test
     void process_mowers_instruction_and_return_mowers_position() throws Exception {
-        String instruction = "5 5 1 2 N LMLMLMLMM 3 3 E MMRMMRMRRM";
-        String position = "1 3 N 5 1 E";
+        String instruction = "{\"instruction\":\"5 5 1 2 N LMLMLMLMM 3 3 E MMRMMRMRRM\"}";
+        String position = "{\"position\": \"1 3 N 5 1 E\"}";
 
         assertRequestWithBody(
                 "POST",
