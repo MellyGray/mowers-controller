f532dd4ec3a03c23a0f5c06352350d1e89202c42
MellyGray <melinaabril@gmail.com>
2022-11-17 20:00:11 +0100

    Add integration test post mowers instruction

---
+++ src/test/java/mowers/controller/controllers/MowersInstructionPostController.java
@@ -0,0 +1,20 @@
+package mowers.controller.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import mowers.controller.RequestTestCase;
+
+public class MowersInstructionPostController extends RequestTestCase{
+    @Test
+    void process_mowers_instruction_and_return_mowers_position() throws Exception {
+        String instruction = "5 5 1 2 N LMLMLMLMM 3 3 E MMRMMRMRRM";
+        String position = "1 3 N 5 1 E";
+
+        assertRequestWithBody(
+                "POST",
+                "/mowers/move",
+                instruction,
+                200,
+                position);
+    }
+}
